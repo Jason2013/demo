dd3d1575f07d483c1daae28105c3d257a8623e67
Jason Chen <shchenchang@gmail.com>
2018-10-29 18:15:28 +0800

    Add echo build.bat

--- build.py
+++ build.py
@@ -10,20 +10,6 @@ VS = os.environ["APPVEYOR_BUILD_WORKER_IMAGE"]
 Config = os.environ["Configuration"]
 Platform = os.environ["Platform"]
 
-  # Visual Studio 15 2017 [arch] = Generates Visual Studio 2017 project files.
-  # Visual Studio 14 2015 [arch] = Generates Visual Studio 2015 project files.
-  # Visual Studio 12 2013 [arch] = Generates Visual Studio 2013 project files.
-  # Visual Studio 11 2012 [arch] = Generates Visual Studio 2012 project files.
-  # Visual Studio 10 2010 [arch] = Generates Visual Studio 2010 project files.
-  # Visual Studio 9 2008 [arch]  = Generates Visual Studio 2008 project files.
-
-# "Visual Studio 15 2017" : "Visual Studio 2017",
-# "Visual Studio 14 2015" : "Visual Studio 2015",
-# "Visual Studio 12 2013" : "Visual Studio 2013",
-# "Visual Studio 11 2012" : "Visual Studio 2012",
-# "Visual Studio 10 2010" : "Visual Studio 2010",
-# "Visual Studio 9 2008" : "Visual Studio 2008",
-
 VS_MAP = {
     "Visual Studio 2017" : "Visual Studio 15 2017",
     "Visual Studio 2015" : "Visual Studio 14 2015",
