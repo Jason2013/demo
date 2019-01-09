c678be0b56249f6cbbe9e984ff3b6503575ce626
Jason Chen <shchenchang@gmail.com>
2019-01-09 19:16:35 +0800

    Change 1727784 by davychen@davychen1x on 2019/01/09 01:30:13
    	SWDEV-109836 - submit MIS source code for Microbench
    
    	1. add admin account to grapher.
    
    Affected files ...
    
    ... //depot/stg/PerfTools/Microbench/tools/Grapher/config/siteInfo.php#2 edit

--- config/siteInfo.php
+++ config/siteInfo.php
@@ -2,6 +2,8 @@
 
 $swtSiteAuthInfo = array("admin_dl", "666888",
                          "admin_yh", "333777",
+                         "admin_zj", "123",
+                         "admin_andy", "123",
                          "admin",    "999666");
 
 ?>
