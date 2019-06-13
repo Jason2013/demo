8a2ad4a8fc0b3724c0f2744686a9ac659be6340f
Jason Chen <shchenchang@gmail.com>
2019-06-13 14:05:38 +0800

    [SERVER] Add `admin_tom` as administrator.

--- config/siteInfo.php
+++ config/siteInfo.php
@@ -4,6 +4,7 @@ $swtSiteAuthInfo = array("admin_dl", "666888",
                          "admin_yh", "333777",
                          "admin_zj", "123",
                          "admin_andy", "123",
+                         "admin_tom", "123",
                          "admin",    "999666");
 
 ?>
