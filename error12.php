f06b7e45a850becdbb7ec8db74dc1c8c1044a940
Chang Chen <Chang.Chen@amd.com>
2019-12-27 17:42:31 +0800

    Call `trigger_error`.

--- error12.php
+++ error12.php
@@ -1,18 +1,20 @@
 <?php
 
-//for ($i =0; $i<10; $i++) { //in range(10)) {
-//    echo $i . "<br />";
-//}
-
-
 //error handler function
-function customError($errno, $errstr)
+function customError($errno, $errstr, $error_file, $error_line)//, $error_context)
 {
-    echo "<b>Error:</b> [$errno] $errstr";
+    echo "<b>Error:</b> [$errno] [$error_file:$error_line][$error_line] $errstr";
 }
 
 //set error handler
 set_error_handler("customError");
 
-//trigger error
-$file = fopen("welcome.txt", "r");
+$a=1;
+$b="hello";
+
+echo "hello, world! $a, \$b</br>";
+
+if ($a < 2)
+{
+    trigger_error("\$a must not less than 2", E_USER_WARNING);
+}
