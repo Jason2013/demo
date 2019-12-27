f972a290d43d6d4ad0215dc59290fd412964298d
Chang Chen <Chang.Chen@amd.com>
2019-12-27 17:51:56 +0800

    Use `try ... catch ...`.

--- error13.php
+++ error13.php
@@ -3,12 +3,16 @@
 //create function with an exception
 function checkNum($number)
 {
-    if($number>1)
-    {
+    if ($number > 1) {
         throw new Exception("Value must be 1 or below");
     }
     return true;
 }
 
+try {
 //trigger exception
-checkNum(2);
+    checkNum(2);
+    echo "No exception";
+} catch (Exception $e) {
+    echo "Exception: " . $e->getMessage();
+}
