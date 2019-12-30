8ccbf52392e9cdebda979de58f30bf5c210e4871
Chang Chen <Chang.Chen@amd.com>
2019-12-30 11:13:41 +0800

    Fix `Unknown database` exception.

--- error14.php
+++ error14.php
@@ -5,7 +5,7 @@ class MyPDO
     function __construct()
     {
         echo "<p>MyPDO::__construct()</p>";
-        $conn = new PDO("mysql:host=localhost;dbname=db_gfxbench", "root", "dgxqh523121");
+        $conn = new PDO("mysql:host=localhost;dbname=db_mis", "root", "dgxqh523120");
         return $conn;
     }
 
