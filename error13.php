d0998a34996c1a63f0570ac44cc86c60166ce243
Chang Chen <Chang.Chen@amd.com>
2019-12-30 14:38:12 +0800

    Add prompt text.

--- error13.php
+++ error13.php
@@ -23,11 +23,15 @@ try {
     $pdoObj = new MyPDO();
     $conn = $pdoObj->getConn();
 
+    echo "<h1>PDO::query and PDOStatement::fetch</h1>";
+
     $stmt = $conn->query("select schema_name, default_character_set_name from information_schema.schemata");
     while ($row = $stmt->fetch())
     {
         echo $row[0] . ", " . $row[1] . "</br>";
     }
+
+    echo "<h1>PDO::prepare and PDOStatement::execute</h1>";
 
     $stmt = $conn->prepare("select schema_name, default_character_set_name from information_schema.schemata where schema_name = ?");
     $stmt->execute(["db_mis"]);
