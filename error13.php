e90ca8c62baeb00927774b2a7b8d921debc62c7b
Chang Chen <Chang.Chen@amd.com>
2020-01-03 13:30:56 +0800

    [STUDY] Set attributes of `PDO`.

--- error13.php
+++ error13.php
@@ -53,6 +53,19 @@ try {
     }
     echo "</table>";
 
+    $attributes = array(
+        "AUTOCOMMIT", "ERRMODE", "CASE", "CLIENT_VERSION", "CONNECTION_STATUS",
+        "ORACLE_NULLS", "PERSISTENT",
+//        "PREFETCH",
+        "SERVER_INFO", "SERVER_VERSION",
+//        "TIMEOUT"
+    );
+
+    foreach ($attributes as $val) {
+        echo "PDO::ATTR_$val: ";
+        echo $conn->getAttribute(constant("PDO::ATTR_$val")) . "</br>";
+    }
+
     echo "No exception";
 } catch (Exception $e) {
     echo "Exception: " . $e->getMessage() . " [CODE]: " . $e->getCode() . " [TRACE]: " . $e->getTraceAsString();
