c8ff8748bb23f9686eec98a49c76b587f603240d
Chang Chen <Chang.Chen@amd.com>
2020-01-20 17:53:35 +0800

    [UNITTEST] Add `SaveVarsToFile` function.

--- phplibs/generalLibs/utils.php
+++ phplibs/generalLibs/utils.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Utilities {
 
     function saveFuncVars($filename, $args)
@@ -23,11 +24,37 @@ namespace Utilities {
         $str = file_get_contents($filename);
         $values = unserialize($str);
 
-        foreach ($values["globals"] as $key => $val){
+        foreach ($values["globals"] as $key => $val) {
             $GLOBALS[$key] = $val;
         }
 
         return $values["args"];
+    }
+
+    function SaveVarsToFile($filename, $headers, $values)
+    {
+        function valueStr($value)
+        {
+            $newValues = Array();
+            foreach ($value as $val) {
+                $str = (string)$val;
+                if (strstr($val, ',') !== false) {
+                    $str = "\"$str\"";
+                }
+                array_push($newValues, $str);
+            }
+            return implode(',', $newValues);
+        }
+
+        if (!file_exists($filename)) {
+            $handle = fopen($filename, "w");
+            fwrite($handle, implode(',', $headers) . "\n");
+            fwrite($handle, valueStr($values) . "\n");
+        } else {
+            $handle = fopen($filename, "a");
+            fwrite($handle, valueStr($values) . "\n");
+        }
+        fclose($handle);
     }
 
 }
